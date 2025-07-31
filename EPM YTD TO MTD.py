409f2a1c6fc61c6f7d975f029850e9e9c1fb97bf
nnteah217 <nguyentheanh21072000@gmail.com>
2025-08-01 02:17:51 +0700

    Update EPM YTD TO MTD.py

--- EPM YTD TO MTD.py
+++ EPM YTD TO MTD.py
@@ -99,10 +99,11 @@ if uploaded_files:
                 df = df[(df["MONTH"] <= CLOSING_M)]   
                 df = df[~((df["EUR AMOUNT"] == 0) & (df["LCC AMOUNT"] == 0))]
 
+                
                 if CURRENCY == "LCC and EUR":
-                    df_final = pd.concat(df[columns_key] , df["LLC AMOUNT","EUR AMOUNT","YEAR","MONTH"],axis = 1)
+                    df_final = pd.concat(df[columns_key] , df["LCC AMOUNT","EUR AMOUNT","YEAR","MONTH"],axis = 1)
                 elif CURRENCY == "LCC only":
-                    df_final = pd.concat(df[columns_key] , df["LLC AMOUNT","YEAR","MONTH"],axis = 1)
+                    df_final = pd.concat(df[columns_key] , df["LCC AMOUNT","YEAR","MONTH"],axis = 1)
                     df_final = df_final[~((df["LCC AMOUNT"] == 0))]
                 elif CURRENCY == "EUR only":
                     df_final = pd.concat(df[columns_key] , df["EUR AMOUNT","YEAR","MONTH"],axis = 1)
