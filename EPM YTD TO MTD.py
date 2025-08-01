e21cf24784f308e21bc895d845e58464ae5e03b8
nnteah217 <nguyentheanh21072000@gmail.com>
2025-08-01 16:00:17 +0700

    Update EPM YTD TO MTD.py

--- EPM YTD TO MTD.py
+++ EPM YTD TO MTD.py
@@ -45,6 +45,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.header("📂 Upload Excel Files to Convert")
     uploaded_files = st.file_uploader("", type=["xlsx"], accept_multiple_files=True)
+    print(len(uploaded_files))
     subcol1, subcol2 = st.columns(2)
     with subcol1:
         CLOSING_M = st.number_input("Input maximum month:",min_value=1, max_value=len(uploaded_files), step=1)
