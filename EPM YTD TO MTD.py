ff05ea0873802a77375c171875455d46aaedce12
nnteah217 <nguyentheanh21072000@gmail.com>
2025-08-01 15:49:43 +0700

    Update EPM YTD TO MTD.py

--- EPM YTD TO MTD.py
+++ EPM YTD TO MTD.py
@@ -47,7 +47,7 @@ with col1:
     uploaded_files = st.file_uploader("", type=["xlsx"], accept_multiple_files=True)
     subcol1, subcol2 = st.columns(2)
     with subcol1:
-        CLOSING_M = st.number_input("Input maximum month:",min_value=1, max_value=12, step=1)
+        CLOSING_M = st.number_input("Input maximum month:",min_value=1, max_value=len(uploaded_files), step=1)
     with subcol2:  
         CURRENCY = st.selectbox("Select currency amount:",["LCC and EUR", "LCC only", "EUR only"])
     run_btn = st.button("🚀 Convert")
